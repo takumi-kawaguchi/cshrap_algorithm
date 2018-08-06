09bfa0a5a5eda6483c3accf40ef5319ff709185d
Takumi Kawaguchi <tcgds4@gmail.com>
2018-08-06 23:52:07 +0900

    Euclid functions

---
+++ SelfCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// here you use any functions you wanna try
+			
+			string stop = Console.ReadLine();
+		}
+	}
+}
---
+++ SelfCSharp/Warmup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp
+{
+	class Warmup
+	{
+		static public void Euclid()
+		{
+			Console.WriteLine("二つの数値を入力してください");
+			var num1 = int.Parse(Console.ReadLine());
+			var num2 = int.Parse(Console.ReadLine());
+
+			while(num1 != num2)
+			{
+				if (num1 > num2)
+				{
+					num1 = num1 - num2;
+				} else
+				{
+					num2 = num2 - num1;
+				}
+			}
+			Console.WriteLine("最大公約数は{0}", num1);
+		}
+
+		static public void Euclid2()
+		{
+			Console.WriteLine("二つの数値を入力して下さい");
+			var num1 = int.Parse(Console.ReadLine());
+			var num2 = int.Parse(Console.ReadLine());
+
+			int k;
+			do
+			{
+				k = num1 % num2;
+				num1 = num2;
+				num2 = k;
+			} while (k != 0);
+
+			Console.WriteLine("最小公約数は{0}", num1);
+		}
+	}
+}
